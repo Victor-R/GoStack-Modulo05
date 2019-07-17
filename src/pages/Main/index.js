7ed1561095daf7eb40f6b9a587cae970dfbf6a41
Victor Ribeiro <victor_ribeiro.pira@hotmail.com>
2019-07-16 23:11:58 -0300

    Finalizado Paginação e Filtros

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,117 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import Container from '../../components/Container';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+import { Form, SubmitButton, List, ErrorMessage } from './styles';
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar Repositório" />
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+    error: {
+      state: false,
+      message: '',
+    },
+  };
 
-        <SubmitButton>
-          <FaPlus color="#fff" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true, error: { state: false, message: '' } });
+
+    try {
+      const { newRepo, repositories } = this.state;
+
+      if (
+        repositories.find(
+          repo => repo.name.toLowerCase() === newRepo.toLowerCase()
+        )
+      ) {
+        throw new Error('Repositório duplicado');
+      }
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        loading: false,
+      });
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: {
+          state: true,
+          message: error.message,
+        },
+      });
+    }
+  };
+
+  render() {
+    const { newRepo, loading, repositories, error } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        {error.state && <ErrorMessage>{error.message}</ErrorMessage>}
+        <Form onSubmit={this.handleSubmit} error={error.state}>
+          <input
+            type="text"
+            placeholder="Adicionar Repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,170 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+import Container from '../../components/Container';
+
+import { Loading, Owner, IssueList, FilterState, PageButtons } from './styles';
+
+export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+    stateFilter: 'open',
+    page: 1,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const { stateFilter, page } = this.state;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: stateFilter,
+          per_page: 5,
+          page,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  async changeFilter(state) {
+    const { match } = this.props;
+    const { page } = this.state;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const response = await api.get(`/repos/${repoName}/issues`, {
+      params: {
+        state,
+        per_page: 5,
+        page,
+      },
+    });
+
+    this.setState({
+      stateFilter: state,
+      issues: response.data,
+    });
+  }
+
+  async changePage(direction) {
+    const { match } = this.props;
+    const { page, state } = this.state;
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const response = await api.get(`/repos/${repoName}/issues`, {
+      params: {
+        state,
+        per_page: 5,
+        page: direction ? page + 1 : page - 1,
+      },
+    });
+
+    console.log(response.data);
+
+    this.setState({
+      page: direction ? page + 1 : page - 1,
+      issues: response.data,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading, stateFilter, page } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <FilterState>
+          <button
+            type="button"
+            disabled={stateFilter === 'open'}
+            onClick={() => this.changeFilter('open')}
+          >
+            Open
+          </button>
+          <button
+            type="button"
+            name="closed"
+            disabled={stateFilter === 'closed'}
+            onClick={() => this.changeFilter('closed')}
+          >
+            Closed
+          </button>
+          <button
+            type="button"
+            name="all"
+            disabled={stateFilter === 'all'}
+            onClick={() => this.changeFilter('all')}
+          >
+            All
+          </button>
+        </FilterState>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+
+        <PageButtons>
+          <button
+            disabled={page === 1}
+            onClick={() => this.changePage(false)}
+            type="button"
+          >
+            <FaChevronLeft color="#7159c1" size={14} />
+          </button>
+
+          <span>Página {page}</span>
+          <button onClick={() => this.changePage(true)} type="button">
+            <FaChevronRight color="#7159c1" size={14} />
+          </button>
+        </PageButtons>
+      </Container>
+    );
+  }
 }
